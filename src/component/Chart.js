78c6293e5797ef922c5f7e34f35a001b96df1200
Aniti <deeptisingh091998@gmail.com>
2021-08-03 14:26:20 +0530

    just check

--- src/component/Chart.js
+++ src/component/Chart.js
@@ -1,4 +1,5 @@
 import ChartBar from "./ChartBar"
+//this is master check
 const Chart = (props) => {
     const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
     const totalMaximum = Math.max(...dataPointValues);
